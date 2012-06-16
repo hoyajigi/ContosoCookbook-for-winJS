7ebaae9f43010a44c45e3b357399fe01527b48bc
Hyunseok Cho <hoyajigi@gmail.com>
2012-06-16 17:02:11 +0900

    Semantic Zoom

--- ContosoCookbook/bin/Debug/AppX/pages/groupedItems/groupedItems.js
+++ ContosoCookbook/bin/Debug/AppX/pages/groupedItems/groupedItems.js
@@ -1,4 +1,74 @@
-﻿(function () {
+﻿//(function () {
+//    "use strict";
+
+//    var appView = Windows.UI.ViewManagement.ApplicationView;
+//    var appViewState = Windows.UI.ViewManagement.ApplicationViewState;
+//    var nav = WinJS.Navigation;
+//    var ui = WinJS.UI;
+//    var utils = WinJS.Utilities;
+
+//    ui.Pages.define("/pages/groupedItems/groupedItems.html", {
+
+//        // This function updates the ListView with new layouts
+//        initializeLayout: function (listView, viewState) {
+//            /// <param name="listView" value="WinJS.UI.ListView.prototype" />
+
+//            if (viewState === appViewState.snapped) {
+//                listView.itemDataSource = Data.groups.dataSource;
+//                listView.groupDataSource = null;
+//                listView.layout = new ui.ListLayout();
+//            } else {
+//                listView.itemDataSource = Data.items.dataSource;
+//                listView.groupDataSource = Data.groups.dataSource;
+//                listView.layout = new ui.GridLayout({ groupHeaderPosition: "top" });
+//            }
+//        },
+
+//        itemInvoked: function (args) {
+//            if (appView.value === appViewState.snapped) {
+//                // If the page is snapped, the user invoked a group.
+//                var group = Data.groups.getAt(args.detail.itemIndex);
+//                nav.navigate("/pages/groupDetail/groupDetail.html", { groupKey: group.key });
+//            } else {
+//                // If the page is not snapped, the user invoked an item.
+//                var item = Data.items.getAt(args.detail.itemIndex);
+//                nav.navigate("/pages/itemDetail/itemDetail.html", { item: Data.getItemReference(item) });
+//            }
+//        },
+
+//        // This function is called whenever a user navigates to this page. It
+//        // populates the page elements with the app's data.
+//        ready: function (element, options) {
+//            var listView = element.querySelector(".groupeditemslist").winControl;
+//            listView.groupHeaderTemplate = element.querySelector(".headerTemplate");
+//            listView.itemTemplate = element.querySelector(".itemtemplate");
+//            listView.oniteminvoked = this.itemInvoked.bind(this);
+
+//            this.initializeLayout(listView, appView.value);
+//            listView.element.focus();
+//        },
+
+//        // This function updates the page layout in response to viewState changes.
+//        updateLayout: function (element, viewState, lastViewState) {
+//            /// <param name="element" domElement="true" />
+//            /// <param name="viewState" value="Windows.UI.ViewManagement.ApplicationViewState" />
+//            /// <param name="lastViewState" value="Windows.UI.ViewManagement.ApplicationViewState" />
+
+//            var listView = element.querySelector(".groupeditemslist").winControl;
+//            if (lastViewState !== viewState) {
+//                if (lastViewState === appViewState.snapped || viewState === appViewState.snapped) {
+//                    var handler = function (e) {
+//                        listView.removeEventListener("contentanimating", handler, false);
+//                        e.preventDefault();
+//                    }
+//                    listView.addEventListener("contentanimating", handler, false);
+//                    this.initializeLayout(listView, viewState);
+//                }
+//            }
+//        }
+//    });
+//})();
+(function () {
     "use strict";
 
     var appView = Windows.UI.ViewManagement.ApplicationView;
@@ -8,24 +78,62 @@
     var utils = WinJS.Utilities;
 
     ui.Pages.define("/pages/groupedItems/groupedItems.html", {
-
+        groupKeySelector: function (item) {
+            return item.group.key;
+        },
+        groupDataSelector: function (item) {
+            return {
+                title: item.group.title,
+                shortTitle: item.group.shortTitle,
+                backgroundImage: item.group.backgroundImage,
+                click: function () {
+                    nav.navigate("/pages/groupDetail/groupDetail.html", { group: item.group });
+                }
+            }
+        },
         // This function updates the ListView with new layouts
         initializeLayout: function (listView, viewState) {
             /// <param name="listView" value="WinJS.UI.ListView.prototype" />
+            var list = Data.items.createGrouped(this.groupKeySelector,
+                this.groupDataSelector);
 
             if (viewState === appViewState.snapped) {
-                listView.itemDataSource = Data.groups.dataSource;
-                listView.groupDataSource = null;
-                listView.layout = new ui.ListLayout();
-            } else {
-                listView.itemDataSource = Data.items.dataSource;
-                listView.groupDataSource = Data.groups.dataSource;
-                listView.layout = new ui.GridLayout({ groupHeaderPosition: "top" });
+                var listViews = document.querySelectorAll(".groupeditemslist");
+
+                for (var i = 0; i < listViews.length; i++) {
+                    var listView = listViews[i].winControl;
+
+                    ui.setOptions(listView, {
+                        itemDataSource: list.groups.dataSource,
+                        groupDataSource: null,
+                        layout: new ui.ListLayout()
+                    });
+                }
+            }
+            else {
+                var zoomedInListView = document.querySelector("#zoomedInListView").winControl;
+
+                ui.setOptions(zoomedInListView, {
+                    itemDataSource: list.dataSource,
+                    groupDataSource: list.groups.dataSource,
+                    layout: new ui.GridLayout({ groupHeaderPosition: "top" })
+                });
+
+                var zoomedOutListView = document.querySelector("#zoomedOutListView").winControl;
+
+                ui.setOptions(zoomedOutListView, {
+                    itemDataSource: list.groups.dataSource,
+                    groupDataSource: null,
+                    layout: new ui.GridLayout({ groupHeaderPosition: "top" })
+                });
             }
         },
 
         itemInvoked: function (args) {
-            if (appView.value === appViewState.snapped) {
+            var zoomedOut = document.querySelector("#zoom").winControl.zoomedOut;
+
+            if (appView.value === appViewState.snapped ||
+                zoomedOut) {
                 // If the page is snapped, the user invoked a group.
                 var group = Data.groups.getAt(args.detail.itemIndex);
                 nav.navigate("/pages/groupDetail/groupDetail.html", { groupKey: group.key });
@@ -39,13 +147,25 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            var listView = element.querySelector(".groupeditemslist").winControl;
-            listView.groupHeaderTemplate = element.querySelector(".headerTemplate");
-            listView.itemTemplate = element.querySelector(".itemtemplate");
-            listView.oniteminvoked = this.itemInvoked.bind(this);
+            var listViews = element.querySelectorAll(".groupeditemslist");
 
-            this.initializeLayout(listView, appView.value);
-            listView.element.focus();
+            for (var i = 0; i < listViews.length; i++) {
+                var listView = listViews[i].winControl;
+
+                ui.setOptions(listView, {
+                    groupHeaderTemplate: document.querySelector(".headerTemplate"),
+                    itemTemplate: document.querySelector(".itemtemplate"),
+                    oniteminvoked: this.itemInvoked
+                });
+
+          //      var appbar = document.querySelector("#appbar").winControl;
+                //appbar.showOnlyCommands(["home"]);
+                //document.querySelector("#home").addEventListener("click", Application.navigateHome, false);
+
+                this.initializeLayout(listView, appView.value);
+                listView.element.focus();
+
+            }
         },
 
         // This function updates the page layout in response to viewState changes.
@@ -54,7 +174,8 @@
             /// <param name="viewState" value="Windows.UI.ViewManagement.ApplicationViewState" />
             /// <param name="lastViewState" value="Windows.UI.ViewManagement.ApplicationViewState" />
 
-            var listView = element.querySelector(".groupeditemslist").winControl;
+            var listView = element.querySelector("#zoomedInListView").winControl;
+
             if (lastViewState !== viewState) {
                 if (lastViewState === appViewState.snapped || viewState === appViewState.snapped) {
                     var handler = function (e) {
